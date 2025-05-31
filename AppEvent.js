6f1d6171abfe15e32cb5c7ccf9ba486e4d10df70
SuaStory <talsofex@gmail.com>
2025-05-31 21:01:35 +0900

    Add files via upload

---
+++ AppEvent.js
@@ -0,0 +1,58 @@
+import './App.css';
+
+
+
+function Article(props) {
+  return <article>
+        <h2>{props.title}</h2>
+        {props.body}
+      </article>
+}
+
+function Header(props) {
+  console.log(props);
+  return <header>
+        <h1><a href="/" onClick={function (event) {
+          event.preventDefault();
+          props.onChangeMode();
+        }}>{props.title}</a></h1>
+      </header>
+}
+
+function Nav(props) {
+  const lis = []
+for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li key={t.id}>
+        <a id={t.id} href={'/read/'+t.id} onClick={function (event) {
+          event.preventDefault();
+          props.onChangeMode(event.target.id);
+        }}>{t.title}</a></li>)
+}
+  return <nav>
+        <ol>
+          {lis}
+        </ol>
+      </nav> 
+}
+
+function App() {
+  const topics = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'java', body:'java is ...'}
+  ]
+  return (
+    <div>
+      <Header title="WEB" onChangeMode={function () {
+        alert('REACT');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={function (id) {
+        alert(id);
+      }}></Nav>
+      <Article title="Welcome!" body="Hello, WEB" ></Article>
+    </div>
+  );
+}
+
+export default App;
